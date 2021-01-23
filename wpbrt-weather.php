18aab6bb481a11b5a93f568599eb660ea3b0f8e4
Sagar Tamang <mi5t4n@gmail.com>
2021-01-23 10:45:11 +0545

    Initial commit

---
+++ wpbrt-weather.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wpbrt Weather
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     WordPress weather plugin.
+ * Author:          wpbrt
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wpbrt-weather
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         WPBRT\\Weather
+ */
+
+// Your code starts here.
